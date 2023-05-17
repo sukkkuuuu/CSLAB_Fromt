dc833ef60803edccb713a0256fe6dcab901bb6f2
sukkkuuuu <sunavy900@gmail.com>
2023-05-17 22:26:37 +0900

    password fetch

--- components/PasswordScreen.js
+++ components/PasswordScreen.js
@@ -1,42 +1,64 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-
+import {
+    Alert,
+    Button,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from "react-native";
+import { find_password } from "./Request";
 const PasswordScreen = () => {
-  const [btntext, setbtn] = useState("확인");
-  const [email, setEmail] = useState("");
-  return (
-    <View style={styles.container}>
-    <Text>비밀번호 찾기</Text>
-    <TextInput 
-        style = {styles.input}
-        onChangeText={(newtext) => setEmail(newtext)}
-        value={email}
-        placeholder="이메일 형식에 맞게 입력하세요."
-      />
-      <Button
-        title={btntext}
-        // onPress={(newText) => setbtn(newText)}
-        // onpress 할 때 이메일 보내는 걸 해야함 => 공부 필요
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [btntext, setbtn] = useState("확인");
+    const [email, setEmail] = useState("");
+    const [accredit, setAccredit] = useState("");
+    const [isHidden, setIsHidden] = useState(true);
+    const handleFindPassword = (email) => {
+        find_password(email).then((res) => console.log(res));
+    };
+    return (
+        <View style={styles.container}>
+            <Text>비밀번호 찾기</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(newtext) => setEmail(newtext)}
+                value={email}
+                placeholder="이메일 형식에 맞게 입력하세요."
+                // 이메일 형식에 맞게 입력 받아야함
+                keyboardType="email-address"
+            />
+            {/* 이메일 인증 되면 hidden 풀기 */}
+            <TextInput
+                style={isHidden ? { display: "none" } : {}}
+                value={accredit}
+                onChangeText={(newtext) => setAccredit(newtext)}
+                placeholder="인증번호 입력하세요."
+            />
+            <TouchableOpacity onPress={() => handleFindPassword(email)}>
+                <Text style={styles.btn}>확인</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    height: 50,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 50,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    input: {
+        height: 50,
+        width: 200,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 50,
+    },
+    btn: {},
 });
 export default PasswordScreen;
